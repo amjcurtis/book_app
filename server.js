27334d6d28326a7c1992bed7533e811b772586d5
perci <Percivaltanner@gmail.com>
2019-02-26 20:28:35 -0800

    added a bunch of scafolding for details page tomorrrow to get foot in right direction

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const superagent = require('superagent');
 require('dotenv').config();
 
 // Application Setup
+
+
+//ADD PG
+
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -17,7 +23,10 @@ app.use(express.urlencoded({ extended: true }));
 // Set file location for EJS templates and static files like CSS
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+//DATABASE SETUP
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err))
 ///////////////////////////////
 // API Routes
 ///////////////////////////////
@@ -35,6 +44,9 @@ app.get('*', (request, response) => response.status(404).send('This route does n
 // Start server listening on port
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
+//get details page
+app.get('/details/:book_details', viewDetails);
+
 ///////////////////////////////
 // MODELS
 ///////////////////////////////
@@ -47,13 +59,30 @@ function Book(res) {
   // TODO Add short-circuit evaluation in case API returns no image
   // TODO Find out why the result chains below were used in the solution code we reviewed in class: 
   // res.imageLinks ? res.imageLinks.smallThumbnail : placeholderImage;
-  this.image_url = res.imageLinks.thumbnail; 
+  this.image_url = res.thumbnail; 
   this.description = res.description; // TODO Add short-circuit fallback in case no description?
 }
 
 ///////////////////////////////
 // HELPER FUNCTIONS
 ///////////////////////////////
+function getSearchHistory(request, response){
+  let SQL = 'SELECT * from tasks;';
+
+  return client.query(SQL)
+    .then(results => response.render('index', {results: results.rows}))
+    .catch(handleError);
+}
+function viewDetails(request, response){
+  let SQL =  'SELECT * FROM tasks WHERE id=$1;';
+  let values = [request.params.'PARAM_GOES_HERE'];
+
+  return client.query(SQL, values)
+    .then(result => {
+      return response.render('pages/book-details', {task:result.rows[0]});
+    })
+    .catch(err => handleError (err,response));
+}
 
 function handleError(err, res) {
   console.error(err);
