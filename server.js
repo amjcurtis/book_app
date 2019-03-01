a60eddec066dc71c0703e6843fe02979a98de7de
amjcurtis <curtis.andrewm@gmail.com>
2019-02-28 18:09:53 -0800

    Make naming more semantic

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ client.on('error', err => console.error(err))
 ///////////////////////////////
 
 // Render homepage
-app.get('/', retrieveDatabaseBooks);
+app.get('/', getBooksFromDatabase);
 
 // Render search form
 app.get('/searches/new', newSearch);
@@ -38,7 +38,7 @@ app.get('/searches/new', newSearch);
 app.post('/searches/show', createSearch); // Listens for post (from html form in index.ejs)
 
 // Route to book details view
-app.get('/books/book-details/:id', viewBookDetails);
+app.get('/books/book-details/:book_id', getOneBook);
 
 // Catch-all for errors (must come after all other routes)
 app.get('*', (request, response) => response.status(404).send('This route does not exist!'));
@@ -52,7 +52,6 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 function Book(res) {
   const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg'; // Attribution: used in solution code we went over in in-class code review
-
   this.title = res.title;
   this.author = res.authors;
   this.image_url = res.imageLinks ? res.imageLinks.smallThumbnail : placeholderImage;
@@ -63,23 +62,24 @@ function Book(res) {
 ///////////////////////////////
 // HELPER FUNCTIONS
 ///////////////////////////////
-function retrieveDatabaseBooks(request, response){
+
+function getBooksFromDatabase(request, response){
   let SQL = 'SELECT * from books;';
   return client.query(SQL)
-    .then(results => response.render('pages/index', {books: results.rows}))
+    .then(results => response.render('pages/index', {books: results.rows})) // NEED TO SPECIFY INDEX OF rows?
     .catch(error => handleError(error, response));
 }
 
-function viewBookDetails(request, response){
+function getOneBook(request, response){
   let SQL =  'SELECT * FROM books WHERE id=$1;';
-  let values = [request.params.id];
+  let values = [request.params.book_id];
   console.log('values', values);
 
   return client.query(SQL, values)
     .then(result => {
       // console.log('single', result.rows[0]);
       return response.render('pages/books/book-details', {books: result.rows});
-    })
+    }) // NEED TO SPECIFY INDEX OF rows?
     .catch(err => handleError (err, response));
 }
 
