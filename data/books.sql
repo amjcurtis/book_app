dffe6df8b22e71c3ed2877c2f79678acba5b44e5
amjcurtis <curtis.andrewm@gmail.com>
2019-02-28 16:17:50 -0800

    Minor improvements to SQL books schema and mock data in books table

--- data/books.sql
+++ data/books.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS books (
   title VARCHAR(255),
   isbn NUMERIC(13,0),
   image_url VARCHAR(255),
-  description VARCHAR(2000),
+  description TEXT,
   bookshelf VARCHAR(255)
 );
 
 INSERT INTO books (author, title, isbn, image_url, description, bookshelf) VALUES ('Kipling', 'Jungle Book', '1234567890123', 'asdfsdfasdfasdf', 'sdfsdfsdfdf', 'asdfsdf') RETURNING id;
-INSERT INTO books (author, title, isbn, image_url, description, bookshelf) VALUES ('Kipling', 'Jungle Book', '1234567890123', 'asdfsdfasdfasdf', 'sdfsdfsdfdf', 'asdfsdf') RETURNING id;
+INSERT INTO books (author, title, isbn, image_url, description, bookshelf) VALUES ('Kipling', 'Just So Stories', '7890567890123', 'dfghdfg', 'sdfgsdfg', 'asdfshjklhjkldf') RETURNING id;
